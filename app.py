b5561796f192e1853a0edc8639b6865be65e41f8
ryazancev.azov <ryazancev.azov@gmail.com>
2024-02-26 20:57:31 +0300

    Завершение выполнения дз

--- app.py
+++ app.py
@@ -4,3 +4,34 @@
 # На странице приветствия должна быть кнопка «Выйти», при нажатии на которую будет удалён cookie-файл с данными
 # пользователя и произведено перенаправление на страницу ввода имени и электронной почты.
 
+from flask import Flask, render_template, request, make_response, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/welcome', methods=['POST'])
+def welcome():
+    user_name = request.form['name']
+    user_email = request.form['email']
+    response = make_response(redirect(url_for('greet')))
+    response.set_cookie('user_name', user_name)
+    response.set_cookie('user_email', user_email)
+    return response
+
+@app.route('/greet')
+def greet():
+    user_name = request.cookies.get('user_name')
+    return render_template('greet.html', user_name=user_name)
+
+@app.route('/logout')
+def logout():
+    response = make_response(redirect(url_for('index')))
+    response.set_cookie('user_name', '', expires=0)
+    response.set_cookie('user_email', '', expires=0)
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
